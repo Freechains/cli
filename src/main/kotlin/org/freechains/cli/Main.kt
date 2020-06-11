6b0ce7e3e64725613e091aa0d0bba8891798e1b1
fsantanna <francisco.santanna@gmail.com>
2020-06-11 20:00:13 -0300

    (>) main functions

--- src/main/kotlin/org/freechains/cli/Main.kt
+++ src/main/kotlin/org/freechains/cli/Main.kt
@@ -4,6 +4,8 @@ import org.freechains.common.*
 import java.io.DataInputStream
 import java.io.DataOutputStream
 import java.io.File
+import java.net.ConnectException
+import java.net.SocketTimeoutException
 import kotlin.system.exitProcess
 
 val help = """
@@ -33,8 +35,9 @@ Usage:
     
 Options:
     --help              [none]            displays this help
-    --version           [none]            displays version information
-    --host=<addr:port>  [all]             sets address and port to connect [default: localhost:$PORT_8330]
+    --version           [none]            displays software version
+    --host=<addr:port>  [all]             sets host address and port to connect [default: localhost:$PORT_8330]
+    --port=port         [all]             sets host port to connect [default: $PORT_8330]
     --sign=<pvt>        [post|(dis)like]  signs post with given private key
     --encrypt           [post]            encrypts post with public key (only in public identity chains)
     --decrypt=<pvt>]    [get]             decrypts post with private key (only in public identity chains)
@@ -44,209 +47,185 @@ More Information:
 
     http://www.freechains.org/
 
-    Please report bugs at <http://github.com/Freechains/core>.
+    Please report bugs at <http://github.com/Freechains/README/>.
 """
 
 fun main (args: Array<String>) {
-    main_cli(args).let { (ok,msg) ->
-        if (ok) {
-            if (msg.isNotEmpty()) {
-                println(msg)
-            }
-        } else {
-            System.err.println(msg)
-            exitProcess(1)
-        }
-    }
+    main_ { main_cli(args) }
 }
 
 fun main_cli_assert (args: Array<String>) : String {
-    return main_cli(args).let { (ok,msg) ->
-        assert_(ok) { msg }
-        msg
-    }
+    return main_assert_ { main_cli(args) }
 }
 
 fun main_cli (args: Array<String>) : Pair<Boolean,String> {
-    val DEF = "freechains-host ${args.joinToString(" ")}"
+    return main_catch_("freechains", VERSION, help, args) { cmds,opts ->
+        val (addr, port_) = (opts["--host"] ?: "localhost:$PORT_8330").to_Addr_Port()
+        val port = opts["--port"]?.toInt() ?: port_
+        val socket = Socket_5s(addr, port)
+        val writer = DataOutputStream(socket.getOutputStream()!!)
+        val reader = DataInputStream(socket.getInputStream()!!)
 
-    return catch_all(DEF) {
-        val (cmds, opts) = args.cmds_opts()
-        //println(">>> $cmds")
-        //println(">>> $opts")
-
-        when {
-            opts.containsKey("--help") -> Pair(true, help)
-            (cmds.size == 0) -> Pair(false, help)
-            else -> {
-                val (addr, port) = (opts["--host"] ?: "localhost:$PORT_8330").to_Addr_Port()
-                val socket = Socket_5s(addr, port)
-                val writer = DataOutputStream(socket.getOutputStream()!!)
-                val reader = DataInputStream(socket.getInputStream()!!)
-
-                @Suppress("UNREACHABLE_CODE")
-                val ret: String = when (cmds[0]) {
-                    "crypto" -> {
+        @Suppress("UNREACHABLE_CODE")
+        val ret: String = when (cmds[0]) {
+            "crypto" -> {
+                assert_(cmds.size == 3)
+                writer.writeLineX("$PRE crypto ${cmds[1]} ${cmds[2]}")
+                reader.readLineX()
+            }
+            "peer" -> {
+                assert_(cmds.size in 3..4)
+                val remote = cmds[1]
+                when (cmds[2]) {
+                    "ping" -> {
                         assert_(cmds.size == 3)
-                        writer.writeLineX("$PRE crypto ${cmds[1]} ${cmds[2]}")
+                        writer.writeLineX("$PRE peer $remote ping")
                         reader.readLineX()
                     }
-                    "peer" -> {
-                        assert_(cmds.size in 3..4)
-                        val remote = cmds[1]
-                        when (cmds[2]) {
-                            "ping" -> {
-                                assert_(cmds.size == 3)
-                                writer.writeLineX("$PRE peer $remote ping")
-                                reader.readLineX()
-                            }
-                            "chains" -> {
-                                assert_(cmds.size == 3)
-                                writer.writeLineX("$PRE peer $remote chains")
-                                reader.readLineX()
-                            }
-                            "send" -> {
-                                assert_(cmds.size == 4)
-                                writer.writeLineX("$PRE peer $remote send ${cmds[3]}")
-                                reader.readLineX()
-                            }
-                            "recv" -> {
-                                assert_(cmds.size == 4)
-                                writer.writeLineX("$PRE peer $remote recv ${cmds[3]}")
-                                reader.readLineX()
-                            }
-                            else -> "!"
-                        }
-                    }
                     "chains" -> {
-                        assert_(cmds.size in 2..4)
-                        when (cmds[1]) {
-                            "list" -> {
-                                assert_(cmds.size == 2)
-                                writer.writeLineX("$PRE chains list")
-                                reader.readLineX()
-                            }
-                            "leave" -> {
-                                assert_(cmds.size == 3)
-                                writer.writeLineX("$PRE chains leave ${cmds[2]}")
-                                reader.readLineX()
-                            }
-                            "join" -> {
-                                assert_(cmds.size in 3..4)
-                                val shared = if (cmds.size == 3) "" else " " + cmds[3]
-                                writer.writeLineX("$PRE chains join ${cmds[2]}$shared")
-                                reader.readLineX()
-                            }
-                            "listen" -> {
-                                assert_(cmds.size == 2)
-                                writer.writeLineX("$PRE chains listen")
-                                while (true) {
-                                    val n_name = reader.readLineX()
-                                    println(n_name)
-                                }
-                                "!"
-                            }
-                            else -> "!"
-                        }
+                        assert_(cmds.size == 3)
+                        writer.writeLineX("$PRE peer $remote chains")
+                        reader.readLineX()
                     }
-                    "chain" -> {
-                        assert_(cmds.size >= 3)
-                        val chain = cmds[1]
-
-                        fun like(lk: String): String {
-                            assert_(cmds.size == 4)
-                            assert_(opts["--sign"] is String) { "expected `--sign`" }
-                            val (len, pay) = opts["--why"].let {
-                                if (it == null) {
-                                    Pair(0, "")
-                                } else {
-                                    Pair(it.length.toString(), it)
-                                }
-                            }
-                            writer.writeLineX("$PRE chain $chain like $lk ${cmds[3]} ${opts["--sign"]} $len")
-                            writer.writeLineX(pay)
-                            return reader.readLineX()
-                        }
-
-                        when (cmds[2]) {
-                            "genesis" -> {
-                                assert_(cmds.size == 3)
-                                writer.writeLineX("$PRE chain $chain genesis")
-                                reader.readLineX()
-                            }
-                            "heads" -> {
-                                assert_(cmds.size == 4)
-                                writer.writeLineX("$PRE chain $chain heads ${cmds[3]}")
-                                reader.readLineX()
-                            }
-                            "get" -> {
-                                assert_(cmds.size == 5)
-                                val decrypt = opts["--decrypt"].toString() // null or pvtkey
-
-                                writer.writeLineX("$PRE chain $chain get ${cmds[3]} ${cmds[4]} $decrypt")
-                                val len = reader.readLineX()
-                                if (len.startsWith('!')) {
-                                    len
-                                } else {
-                                    reader.readNBytesX(len.toInt()).toString(Charsets.UTF_8)
-                                }
-                            }
-                            "post" -> {
-                                assert_(cmds.size in 4..5)
-                                val sign = opts["--sign"] ?: "anon"
-                                val encrypt = opts.containsKey("--encrypt").toString() // null (false) or empty (true)
-
-                                val pay = when (cmds[3]) {
-                                    "inline" -> cmds[4]
-                                    "file" -> File(cmds[4]).readBytes().toString(Charsets.UTF_8)
-                                    "-" -> DataInputStream(System.`in`).readAllBytesX().toString(Charsets.UTF_8)
-                                    else -> error("impossible case")
-                                }
-
-                                writer.writeLineX("$PRE chain $chain post $sign $encrypt ${pay.length}")
-                                writer.writeBytes(pay)
-
-                                reader.readLineX()
-                            }
-                            "traverse" -> {
-                                assert_(cmds.size >= 5)
-                                val downto = cmds.drop(3).joinToString(" ")
-                                writer.writeLineX("$PRE chain $chain traverse ${cmds[3]} $downto")
-                                reader.readLineX()
-                            }
-                            "reps" -> {
-                                assert_(cmds.size == 4)
-                                writer.writeLineX("$PRE chain $chain reps ${cmds[3]}")
-                                reader.readLineX()
-                            }
-                            "like" -> like("1")
-                            "dislike" -> like("-1")
-                            "remove" -> {
-                                assert_(cmds.size == 4)
-                                writer.writeLineX("$PRE chain $chain remove ${cmds[3]}")
-                                assert_(reader.readLineX() == "true")
-                                ""
-                            }
-                            "listen" -> {
-                                assert_(cmds.size == 3)
-                                writer.writeLineX("$PRE chain $chain listen")
-                                while (true) {
-                                    val n = reader.readLineX()
-                                    println(n)
-                                }
-                                "!"
-                            }
-                            else -> "!"
-                        }
+                    "send" -> {
+                        assert_(cmds.size == 4)
+                        writer.writeLineX("$PRE peer $remote send ${cmds[3]}")
+                        reader.readLineX()
+                    }
+                    "recv" -> {
+                        assert_(cmds.size == 4)
+                        writer.writeLineX("$PRE peer $remote recv ${cmds[3]}")
+                        reader.readLineX()
                     }
                     else -> "!"
                 }
-                when {
-                    ret.equals("!") -> Pair(false, "! " + DEF)
-                    ret.startsWith("!") -> Pair(false, ret)
-                    else -> Pair(true, ret)
+            }
+            "chains" -> {
+                assert_(cmds.size in 2..4)
+                when (cmds[1]) {
+                    "list" -> {
+                        assert_(cmds.size == 2)
+                        writer.writeLineX("$PRE chains list")
+                        reader.readLineX()
+                    }
+                    "leave" -> {
+                        assert_(cmds.size == 3)
+                        writer.writeLineX("$PRE chains leave ${cmds[2]}")
+                        reader.readLineX()
+                    }
+                    "join" -> {
+                        assert_(cmds.size in 3..4)
+                        val shared = if (cmds.size == 3) "" else " " + cmds[3]
+                        writer.writeLineX("$PRE chains join ${cmds[2]}$shared")
+                        reader.readLineX()
+                    }
+                    "listen" -> {
+                        assert_(cmds.size == 2)
+                        writer.writeLineX("$PRE chains listen")
+                        while (true) {
+                            val n_name = reader.readLineX()
+                            println(n_name)
+                        }
+                        "!"
+                    }
+                    else -> "!"
                 }
             }
+            "chain" -> {
+                assert_(cmds.size >= 3)
+                val chain = cmds[1]
+
+                fun like(lk: String): String {
+                    assert_(cmds.size == 4)
+                    assert_(opts["--sign"] is String) { "expected `--sign`" }
+                    val (len, pay) = opts["--why"].let {
+                        if (it == null) {
+                            Pair(0, "")
+                        } else {
+                            Pair(it.length.toString(), it)
+                        }
+                    }
+                    writer.writeLineX("$PRE chain $chain like $lk ${cmds[3]} ${opts["--sign"]} $len")
+                    writer.writeLineX(pay)
+                    return reader.readLineX()
+                }
+
+                when (cmds[2]) {
+                    "genesis" -> {
+                        assert_(cmds.size == 3)
+                        writer.writeLineX("$PRE chain $chain genesis")
+                        reader.readLineX()
+                    }
+                    "heads" -> {
+                        assert_(cmds.size == 4)
+                        writer.writeLineX("$PRE chain $chain heads ${cmds[3]}")
+                        reader.readLineX()
+                    }
+                    "get" -> {
+                        assert_(cmds.size == 5)
+                        val decrypt = opts["--decrypt"].toString() // null or pvtkey
+
+                        writer.writeLineX("$PRE chain $chain get ${cmds[3]} ${cmds[4]} $decrypt")
+                        val len = reader.readLineX()
+                        if (len.startsWith('!')) {
+                            len
+                        } else {
+                            reader.readNBytesX(len.toInt()).toString(Charsets.UTF_8)
+                        }
+                    }
+                    "post" -> {
+                        assert_(cmds.size in 4..5)
+                        val sign = opts["--sign"] ?: "anon"
+                        val encrypt = opts.containsKey("--encrypt").toString() // null (false) or empty (true)
+
+                        val pay = when (cmds[3]) {
+                            "inline" -> cmds[4]
+                            "file" -> File(cmds[4]).readBytes().toString(Charsets.UTF_8)
+                            "-" -> DataInputStream(System.`in`).readAllBytesX().toString(Charsets.UTF_8)
+                            else -> error("impossible case")
+                        }
+
+                        writer.writeLineX("$PRE chain $chain post $sign $encrypt ${pay.length}")
+                        writer.writeBytes(pay)
+
+                        reader.readLineX()
+                    }
+                    "traverse" -> {
+                        assert_(cmds.size >= 5)
+                        val downto = cmds.drop(3).joinToString(" ")
+                        writer.writeLineX("$PRE chain $chain traverse ${cmds[3]} $downto")
+                        reader.readLineX()
+                    }
+                    "reps" -> {
+                        assert_(cmds.size == 4)
+                        writer.writeLineX("$PRE chain $chain reps ${cmds[3]}")
+                        reader.readLineX()
+                    }
+                    "like" -> like("1")
+                    "dislike" -> like("-1")
+                    "remove" -> {
+                        assert_(cmds.size == 4)
+                        writer.writeLineX("$PRE chain $chain remove ${cmds[3]}")
+                        assert_(reader.readLineX() == "true")
+                        ""
+                    }
+                    "listen" -> {
+                        assert_(cmds.size == 3)
+                        writer.writeLineX("$PRE chain $chain listen")
+                        while (true) {
+                            val n = reader.readLineX()
+                            println(n)
+                        }
+                        "!"
+                    }
+                    else -> "!"
+                }
+            }
+            else -> "!"
+        }
+        when {
+            ret.startsWith("!") -> Pair(false, ret)
+            else -> Pair(true, ret)
         }
     }
 }
